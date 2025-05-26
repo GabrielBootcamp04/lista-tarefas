0c8739732bb692e933b2a808b630ff5963980e05
Gabriel <gabrielbootcamp04@gmail.com>
2025-05-26 11:15:15 -0300

    commit lista-tarefas

---
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-tarefas',
+  imports: [],
+  templateUrl: './lista-tarefas.component.html',
+  styleUrl: './lista-tarefas.component.css'
+})
+export class ListaTarefasComponent {
+
+}
